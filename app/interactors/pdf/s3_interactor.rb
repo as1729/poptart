ab0a139ffe27c4f84efa454689681934af40c1dd
Aditya Sridhar <aditya@justworks.com>
2019-06-28 11:34:00 -0400

    updated method after testing the functionality.

--- app/interactors/pdf/s3_interactor.rb
+++ app/interactors/pdf/s3_interactor.rb
@@ -1,12 +1,12 @@
 module PDF
   class S3Interactor
-    def self.upload_file(file, session="base_data")
+    def self.upload_file(file_name, file_path, bucket_name=ENV['POPTART_TO_PRINT_BUCKET_NAME'])
       # purely uploads a file and stores it in S3
       s3 = Aws::S3::Client.new
       s3.put_object(
-        bucket: ENV['POPTART_TO_PRINT_BUCKET_NAME'],
-        key: file.original_filename,
-        body: file.tempfile
+        bucket: bucket_name,
+        key: file_name,
+        body: file_path
       )
     end
   end
